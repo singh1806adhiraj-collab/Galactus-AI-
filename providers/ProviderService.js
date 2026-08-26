f0096e00a383fe0f3d5c75d9464a925f58142496
Adhiraj Singh <singh1806adhiraj@gmail.com>
2026-08-26 18:35:52 +0530

    providers have been updated

--- providers/ProviderService.js
+++ providers/ProviderService.js
@@ -36,27 +36,6 @@ function decrypt(encryptedText) {
   return CryptoUtils.decrypt(encryptedText);
 }
 
-// Make encrypt/decrypt available as static methods
-class CryptoUtils {
-  static encrypt(text) {
-    const iv = crypto.randomBytes(IV_LENGTH);
-    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
-    let encrypted = cipher.update(text, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return iv.toString('hex') + ':' + encrypted;
-  }
-
-  static decrypt(encryptedText) {
-    const parts = encryptedText.split(':');
-    const iv = Buffer.from(parts[0], 'hex');
-    const encrypted = parts[1];
-    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
-    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
-  }
-}
-
 class ProviderService {
   async getUserProviders(userId) {
     const db = getDb();
